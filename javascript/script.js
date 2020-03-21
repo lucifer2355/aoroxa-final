9e786598b01d7814dcf0502cd8763dfd96a48989
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-03-21 21:37:55 +0530

    change service page details

---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+if ($.fn.imagesLoaded) {
+  $(".radix-portfolio-filter").imagesLoaded(function() {
+    // init Isotope
+    var $grid = $(".radix-portfolio-filter").isotope({
+      itemSelector: ".single-portfolio-item",
+      filter: ".webDevelopment",
+      percentPosition: true,
+      masonry: {
+        columnWidth: ".single-portfolio-item"
+      }
+    });
+
+    // filter items on button click
+    $(".portfolio-menu li").on("click", "h3", function() {
+      var filterValue = $(this).attr("data-filter");
+      $grid.isotope({
+        filter: filterValue
+      });
+    });
+  });
+}
+
+$(".portfolio-menu li div").on("click", function() {
+  $(".portfolio-menu li div").removeClass("active");
+  $(this).addClass("active");
+});
