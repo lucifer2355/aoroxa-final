4d94bc678752e9c6e1b1b458a03fe01ca281925f
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-04-09 22:31:54 +0530

    fix service page javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,18 @@
 if ($.fn.imagesLoaded) {
-  $('.radix-portfolio-filter').imagesLoaded(function() {
-    var a = $('.radix-portfolio-filter').isotope({
-      itemSelector: '.single-portfolio-item',
-      filter: '.webDevelopment',
+  $(".radix-portfolio-filter").imagesLoaded(function () {
+    var a = $(".radix-portfolio-filter").isotope({
+      itemSelector: ".single-portfolio-item",
+      filter: ".webDevelopment",
       percentPosition: true,
-      masonry: { columnWidth: '.single-portfolio-item' }
+      masonry: { columnWidth: ".single-portfolio-item" },
     });
-    $('.portfolio-menu li').on('click', 'div', function() {
-      var b = $(this).attr('data-filter');
+    $(".portfolio-menu li").on("click", "div", function () {
+      var b = $(this).attr("data-filter");
       a.isotope({ filter: b });
     });
   });
 }
-$('.portfolio-menu li div').on('click', function() {
-  $('.portfolio-menu li div').removeClass('active');
-  $(this).addClass('active');
+$(".portfolio-menu li div").on("click", function () {
+  $(".portfolio-menu li div").removeClass("active");
+  $(this).addClass("active");
 });
